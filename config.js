9837b3b67a7cb232640366505f93e65038ca65ed
bociloptifine <83048701+bociloptifine@users.noreply.github.com>
2021-08-25 10:45:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281324752009', '6281334177340'] // Put your number here
+global.owner = ['6281324752008', '6281334177340'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [6281324752008] // Premium user has unlimited limit
 global.APIs = { // API Prefix
